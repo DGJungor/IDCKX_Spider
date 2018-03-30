1068c024dca90351d2de90726f3c49dea9175309
ZhangJun <568171152@qq.com>
2018-03-30 11:42:12 +0800

    20180330  中午

--- IDCKX_Spider/spiders/IdcquanIndexSpider.py
+++ IDCKX_Spider/spiders/IdcquanIndexSpider.py
@@ -5,82 +5,89 @@ from IDCKX_Spider.items import idcquanindexItem
 
 
 class IdcquanIndexSpider(scrapy.Spider):
-    name = "idcquanindex"
-    allowed_domains = ["idcquan.com"]
-    start_urls = [
-        "http://www.idcquan.com/index/index_1.shtml",
-        # "http://www.idcquan.com/cloud/index6_1.shtml"
-    ]
+	name = "idcquanindex"
+	allowed_domains = ["idcquan.com"]
+	start_urls = [
+		"http://www.idcquan.com/index/index_1.shtml",
+		# "http://www.idcquan.com/cloud/index6_1.shtml"
+	]
 
-    def parse(self, response):
-        for sel in response.xpath("//div[@class='news_con']/div"):
-            # 文章标题
-            title = sel.xpath("div[@class='news_nr']/a/span[@class='title']/text()").extract()
+	def parse(self, response):
+		for sel in response.xpath("//div[@class='news_con']/div"):
+			# 文章标题
+			title = sel.xpath("div[@class='news_nr']/a/span[@class='title']/text()").extract()
 
-            # 获取详情页链接
-            url = sel.xpath("div[@class='news_nr']/a/@href").extract()
+			# 获取详情页链接
+			url = sel.xpath("div[@class='news_nr']/a/@href").extract()
 
-            # 时间
-            date = sel.xpath('span/text()').extract()
+			# 时间
+			date = sel.xpath('span/text()').extract()
 
-            # thumbnail  缩略图
-            thumbnail = sel.xpath('a/img/@src').extract()
+			# thumbnail  缩略图
+			thumbnail = sel.xpath('a/img/@src').extract()
 
-            # 以列表页中的标题链接作为 详情页链接  执行 单页爬取的回调函数
-            # yield scrapy.Request(url[0], callback=self.parse_dir_contents, dont_filter=True)
-            res = scrapy.Request(url[0], callback=self.parse_dir_contents, dont_filter=True)
+			# 以列表页中的标题链接作为 详情页链接  执行 单页爬取的回调函数
+			# yield scrapy.Request(url[0], callback=self.parse_dir_contents, dont_filter=True)
+			res = scrapy.Request(url[0], callback=self.parse_dir_contents, dont_filter=True)
 
-            # 传参 时间
-            res.meta['date'] = date
+			# 传参 时间
+			res.meta['date'] = date
 
-            # 传参 缩略图
-            res.meta['thumbnail'] = thumbnail
+			# 传参 缩略图
+			res.meta['thumbnail'] = thumbnail
 
-            # 传参 文章链接
-            res.meta['url'] = url
+			# 传参 文章链接
+			res.meta['url'] = url
 
-            yield res
+			yield res
 
-        # 获取下一页链接
-        next_pages = response.xpath("//a[@class='next']/@href").extract()
+		# 获取下一页链接
+		next_pages = response.xpath("//a[@class='next']/@href").extract()
 
-        # 判断有无下一页  有则执行回调函数
-        if next_pages:
-            # 对分页链接重新组合
-            # next_page = urljoin(SITE_URL, next_pages[0].extract())
+		# 判断有无下一页  有则执行回调函数
+		if next_pages:
+			# 对分页链接重新组合
+			# next_page = urljoin(SITE_URL, next_pages[0].extract())
 
-            # 以 下一页链接为回调函数参数  重新执行爬取列表页数据
-            yield scrapy.Request(next_pages[0], callback=self.parse, dont_filter=True)
+			# 以 下一页链接为回调函数参数  重新执行爬取列表页数据
+			yield scrapy.Request(next_pages[0], callback=self.parse, dont_filter=True)
 
-    # 对单页详情页 进行数据爬取
-    def parse_dir_contents(self, response):
-        for con in response.xpath("//div[@class='newsbox inner']"):
-            # 实例化
-            item = idcquanindexItem()
+	# 对单页详情页 进行数据爬取
+	def parse_dir_contents(self, response):
+		for con in response.xpath("//div[@class='newsbox inner']"):
+			# 实例化
+			item = idcquanindexItem()
 
-            # 标题
-            item['title'] = con.xpath(
-                "div[@class='article_detail article-infos']/div[@class='title']/text()").extract_first()
+			# 标题
+			item['title'] = con.xpath(
+				"div[@class='article_detail article-infos']/div[@class='title']/text()").extract_first()
 
-            # 关键词
-            # 描述
-            # 来源
-            item['source'] = con.xpath(
-                "div[@class='article_detail article-infos']/div[@class='authorbox clearfix']/div[@class='source']/text()").extract()[
-                0].replace(
-                '\r', '').replace('\n', '').replace('\t', '').replace(' ', '').replace('来源：', '')
+			# 关键词 keywords
+			item['keywords'] = con.xpath("//meta[@name='keywords']/@content").extract_first().replace(' ', ',')
 
-            # 内容
-            item['content'] = \
-                con.xpath("//div[@class='clear deatil article-content fontSizeSmall BSHARE_POP']").extract()[0].replace(
-                    '\r', '').replace('\n', '').replace('\t', '')
+			# 描述 description
+			item['description'] = con.xpath("//meta[@name='description']/@content").extract_first()
 
-            # 日期时间
-            item['date'] = response.meta['date'][0]
+			# 来源
+			item['source'] = con.xpath(
+				"div[@class='article_detail article-infos']/div[@class='authorbox clearfix']/div[@class='source']/text()").extract()[
+				0].replace(
+				'\r', '').replace('\n', '').replace('\t', '').replace(' ', '').replace('来源：', '')
 
-            # 缩略图
-            item['thumbnail'] = response.meta['thumbnail'][0]
+			# 内容
+			item['content'] = \
+				con.xpath("//div[@class='clear deatil article-content fontSizeSmall BSHARE_POP']").extract()[0].replace(
+					'\r', '').replace('\n', '').replace('\t', '')
 
-            print(item['source'])
+			# 日期时间
+			item['date'] = response.meta['date'][0]
 
-            # yield item
+			# 缩略图
+			item['thumbnail'] = response.meta['thumbnail'][0]
+
+			# 文章链接
+			item['url'] = response.meta['url'][0]
+
+			print(item['url'])
+
+		# yield item
