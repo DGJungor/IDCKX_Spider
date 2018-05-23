898a6ace63dbf489e5ba9965a766e854978e6bfd
ZhangJun <568171152@qq.com>
2018-05-23 14:14:51 +0800

    通过改变抓取顺序,以及最后文章时间避免抓取到相同文章,

--- IDCKX_Spider/spiders/idcquanSpider.py
+++ IDCKX_Spider/spiders/idcquanSpider.py
@@ -28,114 +28,135 @@ class idcquanSpider(scrapy.Spider):
             use_unicode=False,
         )
 
-        ttt = dbparams
-        print('=================================================================')
-        print('%s' % (ttt))
-        print('=================================================================')
+        # 查询数据库中最后一篇文章的时间
+        self.post_latest = self.query_post_latest(dbparams)
 
-    # def parse(self, response):
-    # 	for sel in response.xpath("//div[@class='news_con']/div"):
-    # 		# 文章标题
-    # 		title = sel.xpath("div[@class='news_nr']/a/span[@class='title']/text()").extract()
-    #
-    # 		# 获取详情页链接
-    # 		url = sel.xpath("div[@class='news_nr']/a/@href").extract()
-    #
-    # 		# 时间
-    # 		date = sel.xpath('span/text()').extract()
-    #
-    # 		# thumbnail  缩略图
-    # 		thumbnail = sel.xpath('a/img/@src').extract()
-    #
-    # 		# 分类 category
-    # 		category = sel.xpath("div[@class='news_nr']/a/span[@class='tip']/text()").extract()
-    #
-    # 		# 以列表页中的标题链接作为 详情页链接  执行 单页爬取的回调函数
-    # 		# yield scrapy.Request(url[0], callback=self.parse_dir_contents, dont_filter=True)
-    # 		res = scrapy.Request(url[0], callback=self.parse_dir_contents, dont_filter=True)
-    #
-    # 		# 传参 时间
-    # 		res.meta['date'] = date
-    #
-    # 		# 传参 缩略图
-    # 		res.meta['thumbnail'] = thumbnail
-    #
-    # 		# 传参 文章链接
-    # 		res.meta['url'] = url
-    #
-    # 		# 传参 分类
-    # 		res.meta['category'] = category
-    #
-    # 		yield res
-    #
-    # 	# 获取上一页链接
-    # 	next_pages = response.xpath("//a[@class='prev']/@href").extract()
-    #
-    # 	# 获取下一页链接
-    # 	# next_pages = response.xpath("//a[@class='next']/@href").extract()
-    #
-    #
-    # 	# 判断有无下一页  有则执行回调函数
-    # 	if next_pages:
-    # 		# 对分页链接重新组合
-    # 		# next_page = urljoin(SITE_URL, next_pages[0].extract())
-    #
-    # 		# 以 下一页链接为回调函数参数  重新执行爬取列表页数据
-    # 		yield scrapy.Request(next_pages[0], callback=self.parse, dont_filter=True)
-    #
-    # # 对单页详情页 进行数据爬取
-    # def parse_dir_contents(self, response):
-    #
-    # 	# 实例化
-    # 	item = idcquanItem()
-    # 	for con in response.xpath("//div[@class='newsbox inner']"):
-    # 		# 标题
-    # 		item['title'] = con.xpath(
-    # 			"div[@class='article_detail article-infos']/div[@class='title']/text()").extract_first().replace(
-    # 			'\r', '').replace('\n', '').replace('\t', '').replace('<br>', '').replace(' ', '')
-    #
-    # 		# 关键词 keywords
-    # 		item['keywords'] = con.xpath("//meta[@name='keywords']/@content").extract_first().replace(' ', ',')
-    #
-    # 		# 描述 description
-    # 		item['description'] = con.xpath("//meta[@name='description']/@content").extract_first()
-    #
-    # 		# 来源
-    # 		item['source'] = con.xpath(
-    # 			"div[@class='article_detail article-infos']/div[@class='authorbox clearfix']/div[@class='source']/text()").extract_first().replace(
-    # 			'\r', '').replace('\n', '').replace('\t', '').replace(' ', '').replace('来源：', '')
-    #
-    # 		# 内容
-    # 		item['content'] = \
-    # 			con.xpath("//div[@class='clear deatil article-content fontSizeSmall BSHARE_POP']").extract()[
-    # 				0].replace(
-    # 				'\r', '').replace('\n', '').replace('\t', '').replace(
-    # 				' class="clear deatil article-content fontSizeSmall BSHARE_POP"', '')
-    #
-    # 		# 日期时间
-    # 		# item['date'] = time.mktime(response.meta['date'][0])
-    # 		item['date'] = response.meta['date'][0]
-    #
-    # 		# 转时间戳
-    # 		timeArray = time.strptime(item['date'], "%Y-%m-%d %H:%M:%S")
-    # 		item['date'] = int(time.mktime(timeArray))
-    #
-    # 		# 分类
-    # 		item['category'] = response.meta['category'][0]
-    #
-    # 		# 缩略图
-    # 		item['thumbnail'] = response.meta['thumbnail'][0]
-    #
-    # 		# 文章链接
-    # 		item['url'] = response.meta['url'][0]
-    #
-    # 		# print(item['date'])
-    #
-    # 	yield item
+        # print('==============================================================')
+        # print (post_lates)
+        # print('%s' % (ttt))
+        # print('%s' % (dbparams['host']))
+        # print('==============================================================')
 
-    '''
-    根据数据库文章时间数据数据检查重复
-    '''
+    def parse(self, response):
+        for sel in response.xpath("//div[@class='news_con']/div"):
+            # 文章标题
+            title = sel.xpath("div[@class='news_nr']/a/span[@class='title']/text()").extract()
 
+            # 获取详情页链接
+            url = sel.xpath("div[@class='news_nr']/a/@href").extract()
+
+            # 时间
+            date = sel.xpath('span/text()').extract()
+
+            # thumbnail  缩略图
+            thumbnail = sel.xpath('a/img/@src').extract()
+
+            # 分类 category
+            category = sel.xpath("div[@class='news_nr']/a/span[@class='tip']/text()").extract()
+
+            # 以列表页中的标题链接作为 详情页链接  执行 单页爬取的回调函数
+            # yield scrapy.Request(url[0], callback=self.parse_dir_contents, dont_filter=True)
+            res = scrapy.Request(url[0], callback=self.parse_dir_contents, dont_filter=True)
+
+            # 传参 时间
+            res.meta['date'] = date
+
+            # 传参 缩略图
+            res.meta['thumbnail'] = thumbnail
+
+            # 传参 文章链接
+            res.meta['url'] = url
+
+            # 传参 分类
+            res.meta['category'] = category
+
+            yield res
+
+        # 获取上一页链接
+        next_pages = response.xpath("//a[@class='prev']/@href").extract()
+
+        # 获取下一页链接
+        # next_pages = response.xpath("//a[@class='next']/@href").extract()
+
+        # 判断有无下一页  有则执行回调函数
+        if next_pages:
+            # 对分页链接重新组合
+            # next_page = urljoin(SITE_URL, next_pages[0].extract())
+
+            # 以 下一页链接为回调函数参数  重新执行爬取列表页数据
+            yield scrapy.Request(next_pages[0], callback=self.parse, dont_filter=True)
+
+    # 对单页详情页 进行数据爬取
+    def parse_dir_contents(self, response):
+
+        # 实例化
+        item = idcquanItem()
+        for con in response.xpath("//div[@class='newsbox inner']"):
+            # 标题
+            item['title'] = con.xpath(
+                "div[@class='article_detail article-infos']/div[@class='title']/text()").extract_first().replace(
+                '\r', '').replace('\n', '').replace('\t', '').replace('<br>', '').replace(' ', '')
+
+            # 关键词 keywords
+            item['keywords'] = con.xpath("//meta[@name='keywords']/@content").extract_first().replace(' ', ',')
+
+            # 描述 description
+            item['description'] = con.xpath("//meta[@name='description']/@content").extract_first()
+
+            # 来源
+            item['source'] = con.xpath(
+                "div[@class='article_detail article-infos']/div[@class='authorbox clearfix']/div[@class='source']/text()").extract_first().replace(
+                '\r', '').replace('\n', '').replace('\t', '').replace(' ', '').replace('来源：', '')
+            # 如果内容为空则添加来源文字为位置来源
+            if item['source'] == '':
+                item['source'] = '未知来源'
+
+            # 内容
+            item['content'] = \
+                con.xpath("//div[@class='clear deatil article-content fontSizeSmall BSHARE_POP']").extract()[
+                    0].replace(
+                    '\r', '').replace('\n', '').replace('\t', '').replace(
+                    ' class="clear deatil article-content fontSizeSmall BSHARE_POP"', '')
+
+            # 日期时间
+            # item['date'] = time.mktime(response.meta['date'][0])
+            item['date'] = response.meta['date'][0]
+
+            # 转时间戳
+            timeArray = time.strptime(item['date'], "%Y-%m-%d %H:%M:%S")
+            item['date'] = int(time.mktime(timeArray))
+
+            # 分类
+            item['category'] = response.meta['category'][0]
+
+            # 缩略图
+            item['thumbnail'] = response.meta['thumbnail'][0]
+
+            # 文章链接
+            item['url'] = response.meta['url'][0]
+
+        # print(item['date'])
+        if item['date'] > self.post_latest:
+            yield item
+        else:
+            print('文章不符合入库标准')
+
+    # 根据数据库文章时间数据数据检查重复
     def check_repetition(self):
         pass
+
+    # 查询数据库中最新文章时间
+    def query_post_latest(self, dbparams):
+        conn = pymysql.connect(host=dbparams['host'], port=3306, user=dbparams['user'], passwd=dbparams['passwd'],
+                               db=dbparams['db'], charset=dbparams['charset'])
+
+        cursor = conn.cursor()
+        cursor.execute("SELECT idckx_spider_post.date FROM idckx_spider_post ORDER BY date DESC")
+
+        # 获取剩余结果的第一行数据
+        latest_date = cursor.fetchone()
+
+        # 关闭数据库
+        cursor.close()
+        conn.close()
+        return latest_date[0]
--- test.py
+++ test.py
@@ -1,14 +1,21 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+# __author__ = "TKQ"
+import pymysql
 
+conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='tkq1')
+cursor = conn.cursor()
+cursor.execute("select * from tb7")
 
-# if __name__ == "__main__":
-#     print("one.py is being run directly")
-# else:
-#     print("one.py is being imported into another module")
-#
+# 获取剩余结果的第一行数据
+row_1 = cursor.fetchone()
+print row_1
+# 获取剩余结果前n行数据
+# row_2 = cursor.fetchmany(3)
 
-def main():
-    print (123)
+# 获取剩余结果所有数据
+# row_3 = cursor.fetchall()
 
-
-
-# main()
+conn.commit()
+cursor.close()
+conn.close()
